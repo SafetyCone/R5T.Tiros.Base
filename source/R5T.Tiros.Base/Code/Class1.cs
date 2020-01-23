fc1a909e5e8053e0460b3037c0d2be7f900c4d41
MinexAutomation <minex.automation@gmail.com>
2020-01-23 10:19:55 -0800

    Added ITextSerializer<T> service definition interface type.

--- source/R5T.Tiros.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Tiros.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Tiros.Base/Code/Services/Definitions/ITextSerializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+
+namespace R5T.Tiros
+{
+    public interface ITextSerializer<T>
+    {
+        T Deserialize(TextReader reader);
+
+        void Serialize(TextWriter writer, T value);
+    }
+}
